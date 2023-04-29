44459fdeb5cec67e6bbf60344de96f5fb46a0d2b
Niilo Poutanen <niilo.poutanen@gmail.com>
2023-04-29 16:54:35 +0300

    zooming with external lib

--- AndroidApp/app/src/main/java/com/niilopoutanen/rss_feed/ImageViewActivity.java
+++ AndroidApp/app/src/main/java/com/niilopoutanen/rss_feed/ImageViewActivity.java
@@ -8,6 +8,7 @@ import android.widget.ImageView;
 
 import com.niilopoutanen.rss_feed.customization.PreferencesManager;
 import com.niilopoutanen.rss_feed.rss.MaskTransformation;
+import com.ortiz.touchview.TouchImageView;
 import com.squareup.picasso.Picasso;
 
 public class ImageViewActivity extends AppCompatActivity {
@@ -23,8 +24,8 @@ public class ImageViewActivity extends AppCompatActivity {
         if(extras != null){
             url = extras.getString("imageurl");
         }
-        
-        ImageView imageView = findViewById(R.id.imageview);
+
+        TouchImageView imageView = findViewById(R.id.imageview);
 
         Picasso.get().load(url).into(imageView);
     }
