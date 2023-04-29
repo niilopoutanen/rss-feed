e61593f5afa1c1e02a04aee6c416d5fab5a5525b
Niilo Poutanen <niilo.poutanen@gmail.com>
2023-04-29 12:00:35 +0300

    Revert "code cleanup"
    This reverts commit 0a4e8e94588271212628640860d2c721dc00fdcc.

--- AndroidApp/app/src/main/java/com/niilopoutanen/rss_feed/FeedFragment.java
+++ AndroidApp/app/src/main/java/com/niilopoutanen/rss_feed/FeedFragment.java
@@ -3,13 +3,10 @@ package com.niilopoutanen.rss_feed;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.ColorInt;
 import androidx.annotation.NonNull;
@@ -18,21 +15,26 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-import com.niilopoutanen.rss_feed.customization.Preferences;
-import com.niilopoutanen.rss_feed.customization.PreferencesManager;
-import com.niilopoutanen.rss_feed.rss.FeedAdapter;
-import com.niilopoutanen.rss_feed.rss.RSSParser;
-import com.niilopoutanen.rss_feed.rss.RSSPost;
-import com.niilopoutanen.rss_feed.sources.RecyclerViewInterface;
-import com.niilopoutanen.rss_feed.sources.Source;
-import com.niilopoutanen.rss_feed.web.WebCallBack;
-import com.niilopoutanen.rss_feed.web.WebHelper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+
+import com.niilopoutanen.rss_feed.customization.Preferences;
+import com.niilopoutanen.rss_feed.customization.PreferencesManager;
+import com.niilopoutanen.rss_feed.rss.RSSPost;
+import com.niilopoutanen.rss_feed.rss.RSSParser;
+import com.niilopoutanen.rss_feed.rss.FeedAdapter;
+import com.niilopoutanen.rss_feed.sources.RecyclerViewInterface;
+import com.niilopoutanen.rss_feed.sources.Source;
+import com.niilopoutanen.rss_feed.web.WebCallBack;
+import com.niilopoutanen.rss_feed.web.WebHelper;
 
 
 public class FeedFragment extends Fragment implements RecyclerViewInterface {
@@ -46,81 +48,80 @@ public class FeedFragment extends Fragment implements RecyclerViewInterface {
     SwipeRefreshLayout recyclerviewRefresh;
     Preferences preferences;
 
-    @ColorInt
-    int colorAccent;
-
+    @ColorInt int colorAccent;
     public FeedFragment(List<Source> sources, Preferences preferences) {
         this.sources = sources;
         this.preferences = preferences;
     }
-
-    public FeedFragment(Source source, Preferences preferences) {
+    public FeedFragment(Source source, Preferences preferences){
         //single source view
         sources.add(source);
         viewTitle = source.getName();
         this.preferences = preferences;
     }
-
-    public FeedFragment() {
-    }
-
+    public FeedFragment(){}
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         appContext = getContext();
-        if (appContext == null) {
-            return;
+
+        if (savedInstanceState != null) {
+            preferences = (Preferences) savedInstanceState.getSerializable("preferences");
+            sources = (List<Source>) savedInstanceState.getSerializable("sources");
         }
 
-        preferences = savedInstanceState != null ? (Preferences) savedInstanceState.getSerializable("preferences") : null;
-        sources = savedInstanceState != null ? (List<Source>) savedInstanceState.getSerializable("sources") : null;
-
+        assert appContext != null;
         colorAccent = PreferencesManager.getAccentColor(appContext);
-    }
 
+    }
 
     @Override
     public void onItemClick(int position) {
-        RSSPost item = feed.get(position);
-        Intent articleIntent = new Intent(appContext, ArticleActivity.class)
-                .putExtra("postUrl", item.getPostLink())
-                .putExtra("postPublisher", preferences.s_feedcard_authorname ? item.getSourceName() : item.getAuthor())
-                .putExtra("postPublishTime", item.getPublishTime())
-                .putExtra("preferences", preferences);
+        Intent articleIntent = new Intent(appContext, ArticleActivity.class);
+        articleIntent.putExtra("postUrl", feed.get(position).getPostLink());
+        if(!preferences.s_feedcard_authorname){
+            articleIntent.putExtra("postPublisher", feed.get(position).getAuthor());
+        }
+        else{
+            articleIntent.putExtra("postPublisher", feed.get(position).getSourceName());
+        }
+        articleIntent.putExtra("postPublishTime", feed.get(position).getPublishTime());
+
+        articleIntent.putExtra("preferences", preferences);
         appContext.startActivity(articleIntent);
     }
-
 
     @Override
     public void onItemLongClick(int position) {
 
     }
-
-    public void scrollToTop() {
-        if (recyclerView != null) {
+    public void scrollToTop(){
+        if(recyclerView != null){
             recyclerView.smoothScrollToPosition(0);
         }
     }
-
-    private boolean checkValidity() {
-        if (sources.size() <= 0) {
+    interface Callback {
+        void onSuccess();
+    }
+    private boolean checkValidity(){
+        if(sources.size() <= 0){
             Toast.makeText(appContext, appContext.getString(R.string.nosources), Toast.LENGTH_LONG).show();
             return false;
         }
 
         ConnectivityManager connectionManager = appContext.getSystemService(ConnectivityManager.class);
         NetworkInfo currentNetwork = connectionManager.getActiveNetworkInfo();
-        if (currentNetwork == null || !currentNetwork.isConnected()) {
+        if(currentNetwork == null || !currentNetwork.isConnected()){
             Toast.makeText(appContext, appContext.getString(R.string.nointernet), Toast.LENGTH_LONG).show();
             return false;
-        } else {
+        }
+
+        else {
             return true;
         }
     }
-
     private void updateFeed(final Callback callback) {
-        if (recyclerviewRefresh != null) {
+        if(recyclerviewRefresh != null){
             recyclerviewRefresh.setRefreshing(true);
         }
         feed.clear();
@@ -141,11 +142,11 @@ public class FeedFragment extends Fragment implements RecyclerViewInterface {
                             feed.add(post);
                         }
 
+                        Collections.sort(feed);
                     }
                 });
 
             }
-            Collections.sort(feed);
             if (getActivity() != null) {
                 getActivity().runOnUiThread(() -> {
                     if (callback != null) {
@@ -154,6 +155,8 @@ public class FeedFragment extends Fragment implements RecyclerViewInterface {
                 });
             }
         });
+
+
     }
 
     @Override
@@ -161,51 +164,70 @@ public class FeedFragment extends Fragment implements RecyclerViewInterface {
         View rootView = inflater.inflate(R.layout.fragment_feed, container, false);
         recyclerView = rootView.findViewById(R.id.feed_container);
 
-        viewTitle = appContext != null ? appContext.getString(R.string.feed_header) : "Feed";
-        viewTitle = viewTitle.length() > 20 ? viewTitle.substring(0, 20) + "..." : viewTitle;
-
-        adapter = new FeedAdapter(preferences, feed, appContext, viewTitle, this);
+        if(viewTitle == null && appContext != null){
+            viewTitle = appContext.getString(R.string.feed_header);
+        }
+        else if(viewTitle == null){
+            viewTitle = "Feed";
+        }
+        if (viewTitle.length() > 20) {
+            viewTitle = viewTitle.substring(0, 20) + "...";
+        }
+        adapter = new FeedAdapter(preferences ,feed, appContext, viewTitle, this);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(rootView.getContext()));
+
+
 
         recyclerviewRefresh = rootView.findViewById(R.id.recyclerview_refresher);
         recyclerviewRefresh.setColorSchemeColors(colorAccent);
         recyclerviewRefresh.setProgressBackgroundColorSchemeColor(rootView.getContext().getColor(R.color.element));
+        recyclerviewRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                updateFeed(new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        if(adapter != null){
+                            adapter.notifyDataSetChanged();
+                        }
+                        recyclerviewRefresh.setRefreshing(false);
+                    }
+                });
 
-        recyclerviewRefresh.setOnRefreshListener(() -> updateFeed(() -> {
-            adapter.notifyDataSetChanged();
-            recyclerviewRefresh.setRefreshing(false);
-        }));
+            }
+        });
 
-        if (checkValidity()) {
-            updateFeed(() -> {
-                adapter.notifyDataSetChanged();
-                recyclerviewRefresh.setRefreshing(false);
+
+        boolean canLoad = checkValidity();
+        if(canLoad){
+            updateFeed(new Callback() {
+                @Override
+                public void onSuccess() {
+                    adapter.notifyDataSetChanged();
+                    recyclerviewRefresh.setRefreshing(false);
+
+                }
             });
         }
 
+
         return rootView;
     }
-
-
     @Override
     public void onConfigurationChanged(@NonNull Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-        FeedAdapter adapter = (FeedAdapter) recyclerView.getAdapter();
-        if (adapter != null) {
+        FeedAdapter adapter = (FeedAdapter)recyclerView.getAdapter();
+        if(adapter != null){
             adapter.setImageWidth(appContext);
             adapter.notifyDataSetChanged();
         }
-    }
 
+    }
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putSerializable("preferences", preferences);
         outState.putSerializable("sources", new ArrayList<>(sources));
-    }
-
-    interface Callback {
-        void onSuccess();
     }
 }
