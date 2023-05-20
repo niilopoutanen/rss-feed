6668c7ccb66e60d38b043349d7e59c5bbf848a64
Niilo Poutanen <niilo.poutanen@gmail.com>
2023-05-20 12:50:14 +0300

    Fix to parsing

--- AndroidApp/app/src/main/java/com/niilopoutanen/rss_feed/models/FeedResult.java
+++ AndroidApp/app/src/main/java/com/niilopoutanen/rss_feed/models/FeedResult.java
@@ -26,7 +26,7 @@ public class FeedResult {
     public ArrayList<String> topics;
     public String twitterScreenName;
     public int twitterFollowers;
-    public static List<FeedResult> parseResult(String result){
+    public static List<FeedResult> parseResult(String result) {
         List<FeedResult> results = new ArrayList<>();
         try {
             JSONObject jsonObject = new JSONObject(result);
@@ -35,22 +35,55 @@ public class FeedResult {
             for (int i = 0; i < resultsArray.length(); i++) {
                 JSONObject resultObject = resultsArray.getJSONObject(i);
                 FeedResult tempObj = new FeedResult();
-                tempObj.lastUpdated = resultObject.getLong("lastUpdated");
-                tempObj.score = resultObject.getDouble("score");
-                tempObj.language = resultObject.getString("language");
-                tempObj.description = resultObject.getString("description");
-                tempObj.id = resultObject.getString("id");
-                tempObj.title = resultObject.getString("title");
-                tempObj.feedId = resultObject.getString("feedId");
-                tempObj.website = resultObject.getString("website");
-                tempObj.subscribers = resultObject.getInt("subscribers");
-                tempObj.velocity = resultObject.getDouble("velocity");
-                tempObj.updated = resultObject.getLong("updated");
-                tempObj.iconUrl = resultObject.getString("iconUrl");
-                tempObj.partial = resultObject.getBoolean("partial");
-                tempObj.coverUrl = resultObject.optString("coverUrl", null);
-                tempObj.visualUrl = resultObject.getString("visualUrl");
-                tempObj.estimatedEngagement = resultObject.getInt("estimatedEngagement");
+
+                if (resultObject.has("lastUpdated")) {
+                    tempObj.lastUpdated = resultObject.getLong("lastUpdated");
+                }
+                if (resultObject.has("score")) {
+                    tempObj.score = resultObject.getDouble("score");
+                }
+                if (resultObject.has("language")) {
+                    tempObj.language = resultObject.getString("language");
+                }
+                if (resultObject.has("description")) {
+                    tempObj.description = resultObject.getString("description");
+                }
+                if (resultObject.has("id")) {
+                    tempObj.id = resultObject.getString("id");
+                }
+                if (resultObject.has("title")) {
+                    tempObj.title = resultObject.getString("title");
+                }
+                if (resultObject.has("feedId")) {
+                    tempObj.feedId = resultObject.getString("feedId");
+                }
+                if (resultObject.has("website")) {
+                    tempObj.website = resultObject.getString("website");
+                }
+                if (resultObject.has("subscribers")) {
+                    tempObj.subscribers = resultObject.getInt("subscribers");
+                }
+                if (resultObject.has("velocity")) {
+                    tempObj.velocity = resultObject.getDouble("velocity");
+                }
+                if (resultObject.has("updated")) {
+                    tempObj.updated = resultObject.getLong("updated");
+                }
+                if (resultObject.has("iconUrl")) {
+                    tempObj.iconUrl = resultObject.getString("iconUrl");
+                }
+                if (resultObject.has("partial")) {
+                    tempObj.partial = resultObject.getBoolean("partial");
+                }
+                if (resultObject.has("coverUrl")) {
+                    tempObj.coverUrl = resultObject.getString("coverUrl");
+                }
+                if (resultObject.has("visualUrl")) {
+                    tempObj.visualUrl = resultObject.getString("visualUrl");
+                }
+                if (resultObject.has("estimatedEngagement")) {
+                    tempObj.estimatedEngagement = resultObject.getInt("estimatedEngagement");
+                }
 
                 if (resultObject.has("topics")) {
                     JSONArray topicsArray = resultObject.getJSONArray("topics");
@@ -60,6 +93,7 @@ public class FeedResult {
                         tempObj.topics.add(topic);
                     }
                 }
+
                 results.add(tempObj);
             }
         } catch (Exception e) {
@@ -67,4 +101,5 @@ public class FeedResult {
         }
         return results;
     }
+
 }
