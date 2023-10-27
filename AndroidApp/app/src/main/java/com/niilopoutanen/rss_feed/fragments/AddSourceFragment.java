30cb23b3a8e433c63111c93549ca42f7b1f42832
Niilo Poutanen <niilo.poutanen@gmail.com>
2023-10-27 12:13:29 +0300

    Moved fields

--- AndroidApp/app/src/main/java/com/niilopoutanen/rss_feed/fragments/AddSourceFragment.java
+++ AndroidApp/app/src/main/java/com/niilopoutanen/rss_feed/fragments/AddSourceFragment.java
@@ -7,9 +7,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.google.android.material.transition.MaterialSharedAxis;
 import com.niilopoutanen.rss_feed.R;
+import com.niilopoutanen.rss_feed.utils.PreferencesManager;
 
 public class AddSourceFragment extends Fragment {
 
@@ -21,11 +23,20 @@ public class AddSourceFragment extends Fragment {
 
         setEnterTransition(new MaterialSharedAxis(MaterialSharedAxis.X, true));
         setReturnTransition(new MaterialSharedAxis(MaterialSharedAxis.X, false));
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_add_source, container, false);
+
+        LinearLayout returnBtn = rootView.findViewById(R.id.addsource_return);
+        returnBtn.setOnClickListener(view -> closeFragment(returnBtn));
         return rootView;
+    }
+
+    private void closeFragment(View view) {
+        getParentFragmentManager().popBackStack();
+        PreferencesManager.vibrate(view);
     }
 }
