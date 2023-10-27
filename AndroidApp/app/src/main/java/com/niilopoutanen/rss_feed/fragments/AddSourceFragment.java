ea86093ca28b5aa1c8a91fbf94e0f78e571a9be0
Niilo Poutanen <niilo.poutanen@gmail.com>
2023-10-27 12:06:54 +0300

    Initialization

--- AndroidApp/app/src/main/java/com/niilopoutanen/rss_feed/fragments/AddSourceFragment.java
+++ AndroidApp/app/src/main/java/com/niilopoutanen/rss_feed/fragments/AddSourceFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.transition.MaterialSharedAxis;
 import com.niilopoutanen.rss_feed.R;
 
 public class AddSourceFragment extends Fragment {
@@ -18,6 +19,8 @@ public class AddSourceFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setEnterTransition(new MaterialSharedAxis(MaterialSharedAxis.X, true));
+        setReturnTransition(new MaterialSharedAxis(MaterialSharedAxis.X, false));
     }
 
     @Override
