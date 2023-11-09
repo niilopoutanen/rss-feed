47e1196d143e6510b48963c5f0d58c6378e0f142
Niilo Poutanen <niilo.poutanen@gmail.com>
2023-11-09 10:47:56 +0200

    Testing grid LayoutManager

--- AndroidApp/app/src/main/java/com/niilopoutanen/rss_feed/fragments/FeedFragment.java
+++ AndroidApp/app/src/main/java/com/niilopoutanen/rss_feed/fragments/FeedFragment.java
@@ -14,6 +14,7 @@ import android.view.ViewGroup;
 import androidx.annotation.ColorInt;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
@@ -261,8 +262,9 @@ public class FeedFragment extends Fragment implements RecyclerViewInterface {
         }
         adapter = new FeedAdapter(preferences, feed, appContext, viewTitle, this);
         recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(rootView.getContext()));
 
+        final int columns = getResources().getInteger(R.integer.feed_columns);
+        recyclerView.setLayoutManager(new GridLayoutManager(rootView.getContext(), columns));
         recyclerviewRefresh = rootView.findViewById(R.id.recyclerview_refresher);
         recyclerviewRefresh.setColorSchemeColors(colorAccent);
         recyclerviewRefresh.setProgressBackgroundColorSchemeColor(rootView.getContext().getColor(R.color.element));
