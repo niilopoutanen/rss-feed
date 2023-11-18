c1a1d66068438a2ed938b99320bc9b113f6f501f
Niilo Poutanen <niilo.poutanen@gmail.com>
2023-11-18 15:15:39 +0200

    Deprecated categoryveiw

--- app/src/main/java/com/niilopoutanen/rss_feed/activities/DebugActivity.java
+++ app/src/main/java/com/niilopoutanen/rss_feed/activities/DebugActivity.java
@@ -12,7 +12,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.niilopoutanen.rss_feed.BuildConfig;
 import com.niilopoutanen.rss_feed.R;
-import com.niilopoutanen.rss_feed.fragments.CategoryView;
 import com.niilopoutanen.rss_feed.utils.PreferencesManager;
 import com.niilopoutanen.rssparser.Item;
 
--- app/src/main/java/com/niilopoutanen/rss_feed/adapters/DiscoverCategoryAdapter.java
+++ app/src/main/java/com/niilopoutanen/rss_feed/adapters/DiscoverCategoryAdapter.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,7 +11,6 @@ import androidx.appcompat.content.res.AppCompatResources;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.niilopoutanen.rss_feed.R;
-import com.niilopoutanen.rss_feed.fragments.CategoryView;
 import com.niilopoutanen.rss_feed.models.Category;
 
 import java.util.List;
