0e6756f2a5531806a65f8e6ad6b73f9ea1bedf08
Niilo Poutanen <niilo.poutanen@gmail.com>
2023-12-01 12:23:06 +0200

    Test for OPML export

--- app/src/main/java/com/niilopoutanen/rss_feed/models/Source.java
+++ app/src/main/java/com/niilopoutanen/rss_feed/models/Source.java
@@ -3,7 +3,9 @@ package com.niilopoutanen.rss_feed.models;
 import androidx.annotation.Nullable;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 /**
  * Class for saving user's sources to disk
@@ -75,5 +77,25 @@ public class Source implements Serializable {
 
     public void generateId() {
         this.id = UUID.randomUUID();
+    }
+
+
+    public static String generateOPML(List<Source> sources){
+        String opmlHeader = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                  "<opml version=\"1.0\">\n" +
+                  "  <head>\n" +
+                  "    <title>Source List</title>\n" +
+                  "  </head>\n" +
+                  "  <body>\n";
+
+        String opmlSources = sources.stream()
+                  .map(source -> String.format("    <outline text=\"%s\" type=\"rss\" xmlUrl=\"%s\" />\n",
+                            source.getName(), source.getFeedUrl()))
+                  .collect(Collectors.joining());
+
+        String opmlFooter = "  </body>\n" +
+                  "</opml>";
+
+        return opmlHeader + opmlSources + opmlFooter;
     }
 }
