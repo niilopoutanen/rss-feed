32a1af33f827535a8e4b38ed28ca3e8b2e430377
Niilo Poutanen <niilo.poutanen@gmail.com>
2023-12-04 19:53:29 +0200

    Separate RSS module

---
+++ rss/src/main/java/com/niilopoutanen/rss/Source.java
@@ -0,0 +1,20 @@
+package com.niilopoutanen.rss;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class Source {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+    @ColumnInfo(name = "name")
+    public String name;
+    @ColumnInfo(name = "url")
+    public String url;
+    @ColumnInfo(name = "image")
+    public String image;
+
+    @ColumnInfo(name = "visible")
+    public boolean visible;
+}
