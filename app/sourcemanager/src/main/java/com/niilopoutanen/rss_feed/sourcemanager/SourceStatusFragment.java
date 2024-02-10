8aa24bd54dff186de4a7e344484ed3504e4de52a
Niilo Poutanen <niilo.poutanen@gmail.com>
2024-02-10 14:44:30 +0200

    Fragments base

---
+++ app/sourcemanager/src/main/java/com/niilopoutanen/rss_feed/sourcemanager/SourceStatusFragment.java
@@ -0,0 +1,26 @@
+package com.niilopoutanen.rss_feed.sourcemanager;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SourceStatusFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_source_status, container, false);
+
+        return rootView;
+    }
+}
