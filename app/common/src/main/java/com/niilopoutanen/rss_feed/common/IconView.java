f92d4194c01cf69712493b300b89cd01f3daf9ce
Niilo Poutanen <niilo.poutanen@gmail.com>
2024-02-19 18:58:26 +0200

    Building IconView

--- app/common/src/main/java/com/niilopoutanen/rss_feed/common/IconView.java
+++ app/common/src/main/java/com/niilopoutanen/rss_feed/common/IconView.java
@@ -2,25 +2,79 @@ package com.niilopoutanen.rss_feed.common;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.widget.RelativeLayout;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class IconView extends RelativeLayout {
+import androidx.cardview.widget.CardView;
+
+import com.squareup.picasso.Picasso;
+
+public class IconView extends CardView {
+    private ImageView icon;
+    private TextView name;
     public IconView(Context context) {
         super(context);
+        init();
     }
 
     public IconView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public IconView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init(){
+        setCardBackgroundColor(getContext().getColor(R.color.surface));
+        setClipChildren(true);
+
+        setElevation(0);
+
+        icon = new ImageView(getContext());
+        FrameLayout.LayoutParams iconParams = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        icon.setLayoutParams(iconParams);
+        addView(icon);
+
+        name = new TextView(getContext());
+        name.setTextSize(30);
+        FrameLayout.LayoutParams textParams = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        textParams.gravity = Gravity.CENTER;
+        name.setLayoutParams(textParams);
+        addView(name);
     }
 
 
+
+    public void setResource(String url){
+        if(url == null || url.isEmpty()) return;
+        if(icon == null) return;
+        Picasso.get().load(url).into(icon);
+
+        // Clear placeholder text
+        removeView(name);
+        name = null;
+    }
+    public void setName(String nameStr){
+        if(nameStr == null || nameStr.isEmpty()) return;
+        if(icon == null || icon.getDrawable() != null) return;
+        if(name == null) return;
+
+        name.setText(nameStr.substring(0,1));
+    }
     private static double calculateShapeRadius(int width){
         double radiusPercentage = 22.50;
-        double radius = width * (radiusPercentage / 100.0);
-        return radius;
+        return width * (radiusPercentage / 100.0);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        setRadius((float) calculateShapeRadius(w));
     }
 }
--- app/src/main/java/com/niilopoutanen/rss_feed/fragments/components/SourceItem.java
+++ app/src/main/java/com/niilopoutanen/rss_feed/fragments/components/SourceItem.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,20 +19,20 @@ import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.niilopoutanen.rss_feed.activities.FeedActivity;
+import com.niilopoutanen.rss_feed.common.IconView;
 import com.niilopoutanen.rss_feed.common.R;
 import com.niilopoutanen.rss_feed.database.AppRepository;
 import com.niilopoutanen.rss_feed.fragments.AddSourceFragment;
 import com.niilopoutanen.rss_feed.common.models.FeedResult;
 import com.niilopoutanen.rss_feed.common.models.Preferences;
-import com.niilopoutanen.rss_feed.parser.WebUtils;
 import com.niilopoutanen.rss_feed.rss.Source;
 import com.niilopoutanen.rss_feed.common.PreferencesManager;
-import com.squareup.picasso.Picasso;
 
 public class SourceItem extends RecyclerView.ViewHolder {
     private final TextView title;
     private final TextView desc;
-    private final ImageView icon;
+    //private final ImageView icon;
+    private final IconView icon;
     private final RelativeLayout button;
     private final View container;
 
@@ -65,27 +64,21 @@ public class SourceItem extends RecyclerView.ViewHolder {
     public void bindData(Source source, FragmentManager manager) {
         title.setText(source.title);
         desc.setVisibility(View.GONE);
-        loadIcon(source.image);
+        icon.setResource(source.image);
+        icon.setName(source.title);
+
         initButton(source, manager);
     }
 
     public void bindData(FeedResult result) {
         title.setText(result.title);
         desc.setText(result.description);
-        loadIcon(result.visualUrl);
+        icon.setResource(result.visualUrl);
+        icon.setName(result.title);
+
         initButton(result);
     }
 
-
-    private void loadIcon(String iconUrl) {
-        if (iconUrl == null || iconUrl.isEmpty()) {
-            icon.setBackground(AppCompatResources.getDrawable(context, R.drawable.element_background));
-        }
-        int iconSize = PreferencesManager.dpToPx(60, context);
-        Picasso.get().load(iconUrl)
-                  .resize(iconSize, iconSize)
-                  .into(icon);
-    }
 
     private void initButton(Source source, FragmentManager manager) {
         Drawable edit = AppCompatResources.getDrawable(context, R.drawable.icon_edit);
