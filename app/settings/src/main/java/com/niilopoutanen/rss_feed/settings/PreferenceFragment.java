fcac3af63e02f345e04ec8d5466b0fc9bf31e067
Niilo Poutanen <niilo.poutanen@gmail.com>
2024-03-26 19:23:22 +0200

    Testing

--- app/settings/src/main/java/com/niilopoutanen/rss_feed/settings/PreferenceFragment.java
+++ app/settings/src/main/java/com/niilopoutanen/rss_feed/settings/PreferenceFragment.java
@@ -1,4 +1,51 @@
 package com.niilopoutanen.rss_feed.settings;
 
-public class PreferenceFragment {
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.preference.PreferenceFragmentCompat;
+
+import com.niilopoutanen.rss_feed.database.AppViewModel;
+
+public class PreferenceFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
+    AppViewModel appViewModel;
+
+    @NonNull
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        appViewModel = new ViewModelProvider(requireActivity()).get(AppViewModel.class);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
+
+
+        SharedPreferences sharedPreferences = getPreferenceManager().getSharedPreferences();
+        if(sharedPreferences != null){
+            sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        SharedPreferences sharedPreferences = getPreferenceManager().getSharedPreferences();
+        if(sharedPreferences != null){
+            sharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        String value = sharedPreferences.getString(key, null);
+
+
+    }
 }
