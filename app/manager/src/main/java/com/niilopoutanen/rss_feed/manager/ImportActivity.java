8e9fa1d3085aed7c90e23bb543fff6e28a79d542
Niilo Poutanen <niilo.poutanen@gmail.com>
2024-04-13 15:54:30 +0300

    Building more

--- app/manager/src/main/java/com/niilopoutanen/rss_feed/manager/ImportActivity.java
+++ app/manager/src/main/java/com/niilopoutanen/rss_feed/manager/ImportActivity.java
@@ -2,11 +2,15 @@ package com.niilopoutanen.rss_feed.manager;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.Toast;
+import android.widget.TextView;
 
+import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,18 +30,23 @@ import java.util.concurrent.Executors;
 public class ImportActivity extends AppCompatActivity {
 
     private StatusView statusView;
+    private  LinearLayout filePickButton;
+    private ActivityResultLauncher<Intent> fileHandler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_opmlactivity);
+        setContentView(R.layout.activity_import);
 
 
         statusView = findViewById(R.id.activity_import_status_view);
+        filePickButton = findViewById(R.id.activity_import_file_picker);
+
         PrimaryButton primaryButton = findViewById(R.id.activity_import_primary_button);
         primaryButton.setOnClickListener(v -> finish());
 
-        ActivityResultLauncher<Intent> fileHandler = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+        fileHandler = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
             if (result.getResultCode() == Activity.RESULT_OK) {
+                onFileSelected(result);
                 statusView.addStatus(getString(com.niilopoutanen.rss_feed.common.R.string.import_starting));
 
                 Executor executor = Executors.newSingleThreadExecutor();
@@ -58,6 +67,7 @@ public class ImportActivity extends AppCompatActivity {
                     } catch (IOException e) {
                         statusView.addStatus(getString(com.niilopoutanen.rss_feed.common.R.string.error_adding_source), StatusView.Status.Type.FAILURE);
                         FirebaseCrashlytics.getInstance().recordException(e);
+                        reloadFilePicker();
                     }
                 });
             }
@@ -67,15 +77,58 @@ public class ImportActivity extends AppCompatActivity {
         });
 
 
-        LinearLayout filePickButton = findViewById(R.id.activity_import_file_picker);
+        reloadFilePicker();
+    }
+
+    private void onFileSelected(ActivityResult result) {
+        if (filePickButton == null) return;
+        filePickButton.setOnClickListener(null);
+
+        Intent data = result.getData();
+        if (data == null || data.getData() == null) {
+            filePickButton.setVisibility(View.GONE);
+            return;
+        }
+
+        Uri uri = data.getData();
+        TextView fileNameView = filePickButton.findViewById(R.id.activity_import_file_picker_name);
+        String fileName = getFileNameFromUri(uri);
+
+        if (fileName != null && fileNameView != null) {
+            fileNameView.setText(fileName);
+        } else {
+            filePickButton.setVisibility(View.GONE);
+        }
+    }
+
+
+    private String getFileNameFromUri(Uri uri) {
+        if(uri == null || uri.getScheme() == null) return null;
+        String fileName = null;
+        if (uri.getScheme().equals("content")) {
+            try (Cursor cursor = getContentResolver().query(uri, null, null, null, null)) {
+                if (cursor != null && cursor.moveToFirst()) {
+                    int nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+                    if (nameIndex != -1) {
+                        fileName = cursor.getString(nameIndex);
+                    }
+                }
+            }
+            catch (Exception ignored) {}
+        } else if (uri.getScheme().equals("file")) {
+            fileName = uri.getLastPathSegment();
+        }
+        return fileName;
+    }
+
+    private void reloadFilePicker(){
         filePickButton.setOnClickListener(v -> {
             Intent filePicker = new Intent(Intent.ACTION_GET_CONTENT);
             filePicker.setType("*/*");
             filePicker = Intent.createChooser(filePicker, getString(com.niilopoutanen.rss_feed.common.R.string.select_file_import));
-            fileHandler.launch(filePicker);
+            if(fileHandler != null){
+                fileHandler.launch(filePicker);
+            }
         });
-
-
     }
-
 }
